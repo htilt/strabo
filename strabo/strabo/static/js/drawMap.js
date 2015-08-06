b732a32f9fec044a0e8d72a6ceec33111d47f8dd
HankGerba <hank.g@Me.com>
2015-08-06 15:17:19 -0700

    Updated mapDraw functionality

--- strabo/strabo/static/js/drawMap.js
+++ strabo/strabo/static/js/drawMap.js
@@ -1,4 +1,5 @@
-var drawMap = L.map('drawMap', {drawControl: true}, {
+var drawMap = L.map('drawMap', {
+  
   maxBounds: [
   //southWest
   [45.4793, -122.6416],
@@ -19,20 +20,75 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 
 
 
+var drawnItems = new L.FeatureGroup();
+drawMap.addLayer(drawnItems);
+
+// Initialise the draw control and pass it the FeatureGroup of editable layers
+var drawControl = new L.Control.Draw({
+    
+    draw : {
+      polygon: {
+        shapeOptions: {
+          color: 'blue'
+        }
+      },
+      rect: false,
+      polyline: false
+    },
+
+
+    edit: {
+        featureGroup: drawnItems,
+        edit: {
+          selectedPathOptions: {
+            color: 'red'
+        }
+      }
+    }
+});
+drawMap.addControl(drawControl);
+
+var zoneLatLngs;
+
+
 // this is what grabs the coordinate data
 drawMap.on('draw:created', function (e) {
-    var type = e.layerType;
-    var layer = e.layer;
-    var latLngs;
+  var type = e.layerType;
+  var layer = e.layer;
+  var latLngs;
 
-    if (type === 'circle') {
-       latLngs = layer.getLatLng();
-       drawMap.addLayer(layer)
-    }
-    else
-       latLngs = layer.getLatLngs();
-   		drawMap.addLayer(layer);
-       console.log(latLngs); // Returns an array of the points in the path.u75y76yy
+  if (type === 'circle') {
+    latLngs = layer.getLatLng();
+    drawnItems.addLayer(layer);
+  }
+  else if (type === 'marker') {
+    latLngs = layer.getLatLng();
+    drawnItems.addLayer(layer);
+  }
+  else
+    latLngs = layer.getLatLngs();
+    zoneLatLngs = latLngs;
+   	drawnItems.addLayer(layer);
+    console.log(latLngs);
+  })
+
+  drawMap.on('draw:edited', function (e) {
+
+  var editLayers = e.layers;
+  var type = e.layerType;
+
+  editLayers.eachLayer(function (layer) {
+    var editCoords = layer.getLatLngs();
+    zoneLatLngs = editCoords;
+    console.log(zoneLatLngs);
+    console.log(zoneLatLngs[0].lat);
+        //do whatever you want, most likely save back to db
+    });
+
+})
+  // Returns an array of the points in the path.
+
+
+       // need to write a function which updates this array if the points are edited.
 
     // process latLngs as you see fit and then save
-})
