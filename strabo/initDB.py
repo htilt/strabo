7e174c83b38756d5d4aa6aa62d7caaf889eb8997
Haley Tilt <htilt@reed.edu>
2015-08-22 10:06:36 -0700

    pulled in changes made on local livy branch, altered them to be canyon specific.

--- strabo/initDB.py
+++ strabo/initDB.py
@@ -3,14 +3,14 @@ from contextlib import closing
 
 # This function starts a new connection to a database.
 def conn_db():
-  conn = sqlite3.connect('livy.sqlite3')
+  conn = sqlite3.connect('bbs.sqlite3')
   return conn
 
 # This function loads in the proper sql table if it doesn't already exist.
 def intiate_db():
-  if not os.path.exists('livy.sqlite3'):
+  if not os.path.exists('bbs.sqlite3'):
     with closing(conn_db()) as db:
-      with open("strabo/schema-livy.sql", mode='r') as fh:
+      with open("strabo/schema.sql", mode='r') as fh:
         db.cursor().executescript(fh.read())
         db.commit()
 
