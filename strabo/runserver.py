c62cee3b85ab1c652f09d865eb477963223b91bd
benblack <weepingwillowben@gmail.com>
2016-06-27 17:49:57 -0700

    organized some files

--- strabo/runserver.py
+++ strabo/runserver.py
@@ -3,4 +3,4 @@ from strabo import app
 if __name__ == '__main__':
   app.secret_key = 'A0Zr98j/3yXdksjghlaehg098p987__)(O(&&*^nSDVf/.,][pasdfb,?RT'
   app.debug = True # turn this to False when it is in production mode
-  app.run(host)
+  app.run()
