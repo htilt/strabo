ba4b7b483205a6697341fc056855177d3cd94e49
Rachel Kennelly <kennelra@Steve-the-MacBook-Pro.local>
2016-07-18 12:14:05 -0700

    updated pin file names

--- strabo/add_database.py
+++ strabo/add_database.py
@@ -69,8 +69,8 @@ db.session.commit()
 
 ip3_description = "Reed Lake was created sometime near the turn of the century by the construction of a 10-foot-high dam across Reed Creek. The creation of the dam blocked fish passage, as the creek was re-routed through a culvert system that discharged to a steep waterfall. \n \n Over the summer and fall of 2001, Reed College constructed a fish ladder to re-establish connectivity between Reed Lake and the lower creek for resident and anadromous fish."
 
-ip1 = make_interest_point([1,2,3],"Interest Point 1","This is a descriptions of something",geo_obj1,app.config['LAYER_FIELDS'][0],"Navy.png")
-ip2 = make_interest_point([4],"Interest Point 2","This is a descriptions of something else",geo_obj2,app.config['LAYER_FIELDS'][1],"Green.png")
+ip1 = make_interest_point([1,2,3],"Interest Point 1","This is a descriptions of something",geo_obj1,straboconfig['LAYER_FIELDS'][0],"Navy.png")
+ip2 = make_interest_point([4],"Interest Point 2","This is a descriptions of something else",geo_obj2,straboconfig['LAYER_FIELDS'][1],"Green.png")
 
 
 db.session.commit()
