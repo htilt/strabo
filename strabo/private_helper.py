5c0a1b91ef9177965fdf4adfdec70cd1c0a9e9ef
benblack <weepingwillowben@gmail.com>
2016-08-09 09:13:12 -0700

    date revert and image edit fix

--- strabo/private_helper.py
+++ strabo/private_helper.py
@@ -24,32 +24,18 @@ def fill_interest_point(ip,image_ids,form_title,form_body,form_geo_obj,form_laye
     ip.icon = form_icon
     ip.images = [db.session.query(schema.Images).get(int(id)) for id in image_ids]
 
-def make_date(form_year,form_month):
-    '''
-    :param string form_year: Year value passed in from form. Is a string decimal
-        representation of a year or en empty string if that part of the form was
-        left empty.
-
-    :param string form_month: Month value, follows same format as year.
-
-    If month and year are valid values,returns a date with specied month and year.
-
-    If not, then it uses today's date as a default.
-    '''
-    default_day = 1
-    year = int(form_year) if form_year else datetime.date.today().year
-    month = int(form_month) if form_month else datetime.date.today().month
-
-    return  datetime.date(year,month,default_day)
-
 def fill_image(image,form_file_obj,form_descrip,year,month):
     '''
-    if a flask.files object is passed in, then
+    If a flask.files object is passed in, then it saves the new file and stores the new information in the database.
+    If the image row already stored infromation about an old image (the image was edited), then that old image is deleted.
     '''
-    image.taken_at = make_date(year,month)
+    image.taken_at = datetime.date(utils.safe_pos_int_conv(year),utils.safe_pos_int_conv(month),1)
     image.description = form_descrip
 
     if form_file_obj:
+        if image.filename:
+            image_processing.delete_image_files(image.filename)
+
         image.filename = file_writing.make_filename(form_file_obj.filename)
         file_writing.save_image_files(form_file_obj,image.filename)
         image.width,image.height = image_processing.get_dimentions(image.filename)
