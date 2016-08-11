65c4ece0e8472ab0ea2e4a8ae522367977f5e237
benblack <weepingwillowben@gmail.com>
2016-08-10 18:02:50 -0700

    refractored and enhanced edit interface to make it more consistent with upload interface

--- strabo/static/js/drawMap.js
+++ strabo/static/js/drawMap.js
@@ -1,101 +1,112 @@
-// The Admin Map
-
 var shape_drawn = false;
 var shapeLayer;
 
+function init_map(){
+    /*
+    Creates and configures the admin map and adds the interest points to it.
 
+    Returns the map.
+    */
+    var drawMap = make_map('drawMap');
+    add_tile_to(drawMap);
+
+
+    var all_layers_group = L.geoJson(features);
+    set_styles(all_layers_group);
+    bind_popups(all_layers_group);
+    all_layers_group.addTo(drawMap);
+    return drawMap;
+}
+function set_draw_controls(drawMap,drawnItems){
+    /*
+    Sets controls which allow one to add a polygon or a point. The control mechanics
+    have two.
+    */
+    // Initialise the draw control and pass it the FeatureGroup of editable layers
+
+    var shapeColorInit = '#2397EB';
+
+    var addControl = new L.Control.Draw({
+        draw : {
+          polyline: false,
+          polygon: {
+            shapeOptions: {
+              color: shapeColorInit
+            }
+          },
+          circle: false,
+          rectangle: false
+        },
+
+
+        edit: {
+          featureGroup: drawnItems,
+          edit: {
+            selectedPathOptions: {
+              color: 'red'
+            }
+          }
+        }
+    });
+
+    var editControl = new L.Control.Draw({
+      draw: false,
+
+      edit: {
+          featureGroup: drawnItems,
+          edit: {
+            selectedPathOptions: {
+              color: 'red'
+            }
+          }
+        }
+    });
+    if(edit_json){
+        drawMap.addControl(editControl);
+    }
+    else{
+        drawMap.addControl(addControl);
+    }
+
+
+    drawMap.on('draw:created', function (e) { //grab s layer of drawn item
+        shapeLayer = e.layer;
+        shape_drawn = true;
+        drawnItems.addLayer(shapeLayer);
+
+        addControl.removeFrom(drawMap);
+        drawMap.addControl(editControl);
+    })
+
+    drawMap.on('draw:deleted', function (e) {
+        //hackish way of finding out if there were any objects
+        //that were actually deleted. Tested with leaflet 0.7.7 and leaflet-draw 0.2.3
+        //probably should be retested on package upgrade
+        var objs_deleted = Object.keys(e.layers._layers);
+        if (objs_deleted.length > 0){
+            shape_drawn = false;
+
+            editControl.removeFrom(drawMap);
+            drawMap.addControl(addControl);
+        }
+    })
+}
+function init_geojson_setter(drawnItems){
+    if (edit_json){
+        shape_drawn = true;
+        shapeLayer = L.geoJson(edit_json).getLayers()[0];
+        shapeLayer.addTo(drawnItems);
+    }
+    $('#upload-btn').click(function (e) {
+        var JSONobject = shape_drawn ? JSON.stringify(shapeLayer.toGeoJSON()) : "";
+        $('#geojson-field').attr("value", JSONobject);
+    });
+}
 $(function(){
-var drawMap = make_map('drawMap');
-add_tile_to(drawMap);
+    var drawMap = init_map();
 
-var all_layers_group = L.geoJson(features);
-set_styles(all_layers_group);
-bind_popups(all_layers_group);
-all_layers_group.addTo(drawMap);
-
-var drawnItems = new L.FeatureGroup();
-drawMap.addLayer(drawnItems);
-
-// Initialise the draw control and pass it the FeatureGroup of editable layers
-// Removes some toolbar things and also sets colors
-
-var shapeColorInit = '#2397EB';
-
-if (edit_json){
-    shape_drawn = true;
-    shapeLayer = L.geoJson(edit_json).getLayers()[0];
-}
-
-var options1 = {
-
-    draw : {
-      polyline: false,
-      polygon: {
-        shapeOptions: {
-          color: shapeColorInit
-        }
-      },
-      circle: false,
-      rectangle: false
-    },
-
-
-    edit: {
-      featureGroup: drawnItems,
-      edit: {
-        selectedPathOptions: {
-          color: 'red'
-        }
-      }
-    }
-}
-
-var options2 = {
-  draw: false,
-
-  edit: {
-      featureGroup: drawnItems,
-      edit: {
-        selectedPathOptions: {
-          color: 'red'
-        }
-      }
-    }
-}
-
-var drawControla = new L.Control.Draw(options1);
-var drawControlb = new L.Control.Draw(options2);
-
-drawMap.addControl(drawControla);
-
-drawMap.on('draw:created', function (e) { //grab s layer of drawn item
-    shapeLayer = e.layer;
-    shape_drawn = true;
-    drawnItems.addLayer(shapeLayer);
-
-    drawControla.removeFrom(drawMap);
-    drawMap.addControl(drawControlb);
-})
-
-drawMap.on('draw:deleted', function (e) {
-    //awkward, test this in different browsers
-    if (Object.keys(e.layers._layers).length > 0){
-        shape_drawn = false;
-
-        drawControlb.removeFrom(drawMap);
-        drawMap.addControl(drawControla);
-    }
-})
-/*
-  var $e = $("#colorPick")
-  var $usrSelect = $("#colorPick :selected").text();
-
-  $e.change(function() {
-    $usrSelect = $("#colorPick :selected").text();
-});*/
-});
-
-$('#upload-btn').click(function (e) {
-    var JSONobject = JSON.stringify(shapeLayer.toGeoJSON());
-    $('#geojson-field').attr("value", JSONobject);
+    var drawnItems = new L.FeatureGroup();
+    drawMap.addLayer(drawnItems);
+    init_geojson_setter(drawnItems)
+    set_draw_controls(drawMap,drawnItems)
 });
